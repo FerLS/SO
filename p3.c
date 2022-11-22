4d0854f0941d342c8ce935fb9c58a9d0976fbdb3
braisf03 <brais.sferreiro@udc.es>
2022-11-22 18:27:22 +0100

    mmap

---
+++ p3.c
@@ -0,0 +1,5 @@
+//
+// Created by brais on 22/11/22.
+//
+
+#include "p3.h"
---
+++ p3.h
@@ -0,0 +1,8 @@
+//
+// Created by brais on 22/11/22.
+//
+
+#ifndef PRACTICA1_P3_H
+#define PRACTICA1_P3_H
+
+#endif //PRACTICA1_P3_H
